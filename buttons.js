e2fd5f1e9338313a76f30fb43de5ec31b93b85b9
alia6170 <49277340+alia6170@users.noreply.github.com>
2019-04-04 08:37:44 -0400

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,10 @@
+function about(){
+	window.location.href = "about.html"
+}
+function highlighter(){
+	window.location.href = "highlighter.html"
+	
+}
+function contact(){
+	window.location.href = "contact.html"
+}

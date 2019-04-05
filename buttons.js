eba5141b89e017a1165350fa85ff2bf8601e217b
BlazingCrumpet <45927702+BlazingCrumpet@users.noreply.github.com>
2019-04-05 08:19:56 -0400

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -8,3 +8,14 @@ function highlighter(){
 function contact(){
 	window.location.href = "contact.html"
 }
+function w3_open() {
+  document.getElementById("main").style.marginLeft = "180px";
+  document.getElementById("mySidebar").style.width = "180px";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+function w3_close() {
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
